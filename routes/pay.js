d5e3e015dc771d21b58562af58fc241b049b0778
Bétourné Alexandre <alexandre.betourne@hotmail.fr>
2018-07-12 15:02:40 +0200

    error payment'

--- routes/pay.js
+++ routes/pay.js
@@ -17,56 +17,22 @@ router.post('/', function(req, res, next) {
 	);
 	const token = req.body.id_token;
 	var price = req.body.price.split('.').join("")
-	// stripe.customers.create({
-	// 	description: 'Customer for sophia.brown@example.com',
-	// 	source: token
-	// }, function(err, customer) {
-	// 	switch (err.type) {
-	// 		case 'StripeCardError':
-	// 			res.render('pay', {
-	// 				error: err.message
-	// 			});
-	// 			break;
-	// 		case 'RateLimitError':
-	// 			res.render('pay', {
-	// 				error: err.message
-	// 			});
-	// 			break;
-	// 		case 'StripeInvalidRequestError':
-	// 			res.render('pay', {
-	// 				error: err.message
-	// 			});
-	// 			break;
-	// 		case 'StripeAPIError':
-	// 			res.render('pay', {
-	// 				error: err.message
-	// 			});
-	// 			break;
-	// 		case 'StripeConnectionError':
-	// 			res.render('pay', {
-	// 				error: err.message
-	// 			});
-	// 			break;
-	// 		case 'StripeAuthenticationError':
-	// 			res.render('pay', {
-	// 				error: err.message
-	// 			});
-	// 			break;
-	// 		default:
-	// 			res.render('pay', {
-	// 				error: err.message
-	// 			});
-	// 			break;
-	// 	}
-	// })
-
 	stripe.charges.create({
 		amount: price,
 		currency: 'eur',
 		source: token,
 		receipt_email: 'alexandre.betourne.pro@gmail.com'
-	});
-	res.redirect('/confirm');
+	}, function(err) {
+		if (err) {
+			res.render('pay', {
+				user: req.session.user.connected,
+				error: err.message
+			});
+		} else {
+			res.redirect('/confirm');
+		}
+	})
+
 });
 
 module.exports = router;
